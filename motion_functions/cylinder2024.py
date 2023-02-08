a975e26d749da2fd5c4fc39542d016ce886b2f4b
Nathan A. Wukie <nathan.wukie@us.af.mil>
2023-02-07 22:34:29 -0500

    Updated python motion function.

--- motion_functions/cylinder2024.py
+++ motion_functions/cylinder2024.py
@@ -47,7 +47,7 @@ def thetag(t,r0,theta0):
     return theta0 + Ag*fg(t,r0,theta0)
 
 
-def coords_case1(x0,y0,t):
+def coords(alpha,x0,y0,t):
 
     r0     = cmath.sqrt(x0*x0 + y0*y0)
     theta0 = c_atan2(y0,x0)
@@ -55,26 +55,30 @@ def coords_case1(x0,y0,t):
     xdef0 = r0*cmath.cos(thetag(t,r0,theta0))
     ydef0 = r0*cmath.sin(thetag(t,r0,theta0))
 
+    x = cmath.cos(Atheta*alpha(t))*psi(t,alpha)*xdef0 - (cmath.sin(Atheta*alpha(t))/psi(t,alpha))*ydef0
+    y = cmath.sin(Atheta*alpha(t))*psi(t,alpha)*xdef0 + (cmath.cos(Atheta*alpha(t))/psi(t,alpha))*ydef0 + alpha(t)
+
     # cmath call is always converting things into complex, so we turn things back
     # into floats if inputs are floats.
     if isinstance(x0,complex) or isinstance(y0,complex) or isinstance(t,complex):
-        x_return = xdef0
-        y_return = ydef0
+        x_return = x
+        y_return = y
     else:
-        x_return = xdef0.real
-        y_return = ydef0.real
+        x_return = x.real
+        y_return = y.real
 
     return x_return, y_return
 
 
-def coords_dot(coords,x0,y0,t):
+def coords_dot(alpha,x0,y0,t):
 
     h = 1.e-30
     eps = complex(0.,h)
 
-    dxdx,dydx = coords(x0+eps,y0    ,t    )
-    dxdy,dydy = coords(x0    ,y0+eps,t    )
-    dxdt,dydt = coords(x0    ,y0    ,t+eps)
+    # Evaluate function with complex perturbations in x,y,t
+    dxdx,dydx = coords(alpha,x0+eps,y0    ,t    )
+    dxdy,dydy = coords(alpha,x0    ,y0+eps,t    )
+    dxdt,dydt = coords(alpha,x0    ,y0    ,t+eps)
 
     # Divide imaginary part by step size
     dxdx = dxdx.imag/h
@@ -87,40 +91,42 @@ def coords_dot(coords,x0,y0,t):
     return dxdx, dxdy, dxdt, dydx, dydy, dydt
 
 
-x0 = 1.
-y0 = 0.3
-t = 1.
+def test_derivatives():
+
+    x0 = 1.
+    y0 = 0.3
+    t = 1.
+    fd_eps = 1.e-9
+
+    # Compute complex-step derivatives
+    dxdx_cs, dxdy_cs, dxdt_cs, dydx_cs, dydy_cs, dydt_cs = coords_dot(alpha1,x0,y0,t)
+
+    # Compute finite-difference derivatives
+    xa,ya         = coords(alpha1,x0,y0,t)
+    x_xdot,y_xdot = coords(alpha1,x0+fd_eps,y0       ,t       )
+    x_ydot,y_ydot = coords(alpha1,x0       ,y0+fd_eps,t       )
+    x_tdot,y_tdot = coords(alpha1,x0       ,y0       ,t+fd_eps)
+
+    dxdx_fd = (x_xdot - xa)/fd_eps
+    dydx_fd = (y_xdot - ya)/fd_eps
+
+    dxdy_fd = (x_ydot - xa)/fd_eps
+    dydy_fd = (y_ydot - ya)/fd_eps
+
+    dxdt_fd = (x_tdot - xa)/fd_eps
+    dydt_fd = (y_tdot - ya)/fd_eps
 
 
-dxdx_cs, dxdy_cs, dxdt_cs, dydx_cs, dydy_cs, dydt_cs = coords_dot(coords_case1,x0,y0,t)
+    print("{: >30} {: >30} {: >30}".format(*[f'Finite-difference(step={fd_eps})', 'Complex-step', 'Error']))
+    print("{: >30} {: >30} {: >30}".format(*[dxdx_fd, dxdx_cs, dxdx_fd - dxdx_cs]))
+    print("{: >30} {: >30} {: >30}".format(*[dxdy_fd, dxdy_cs, dxdy_fd - dxdy_cs]))
+    print("{: >30} {: >30} {: >30}".format(*[dxdt_fd, dxdt_cs, dxdt_fd - dxdt_cs]))
+    print("{: >30} {: >30} {: >30}".format(*[dydx_fd, dydx_cs, dydx_fd - dydx_cs]))
+    print("{: >30} {: >30} {: >30}".format(*[dydy_fd, dydy_cs, dydy_fd - dydy_cs]))
+    print("{: >30} {: >30} {: >30}".format(*[dydt_fd, dydt_cs, dydt_fd - dydt_cs]))
 
 
-
-fd_eps = 1.e-8
-
-xa,ya         = coords_case1(x0,y0,t)
-x_xdot,y_xdot = coords_case1(x0+fd_eps,y0       ,t       )
-x_ydot,y_ydot = coords_case1(x0       ,y0+fd_eps,t       )
-x_tdot,y_tdot = coords_case1(x0       ,y0       ,t+fd_eps)
-
-dxdx_fd = (x_xdot - xa)/fd_eps
-dydx_fd = (y_xdot - ya)/fd_eps
-
-dxdy_fd = (x_ydot - xa)/fd_eps
-dydy_fd = (y_ydot - ya)/fd_eps
-
-dxdt_fd = (x_tdot - xa)/fd_eps
-dydt_fd = (y_tdot - ya)/fd_eps
-
-
-print(dxdx_fd, dxdx_cs, dxdx_fd - dxdx_cs)
-print(dxdy_fd, dxdy_cs, dxdy_fd - dxdy_cs)
-print(dxdt_fd, dxdt_cs, dxdt_fd - dxdt_cs)
-print(dydx_fd, dydx_cs, dydx_fd - dydx_cs)
-print(dydy_fd, dydy_cs, dydy_fd - dydy_cs)
-print(dydt_fd, dydt_cs, dydt_fd - dydt_cs)
-
-
+test_derivatives()
 
 
 
